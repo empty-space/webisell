df03fe1a85191a66623f0f68ff3d5033031200ec
empty-space <zergostanIND31@gmail.com>
2018-11-14 14:47:27 +0200

    Created Website project

---
+++ Webisell.Web/Webisell.Web/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Webisell.Web.ViewModels;
+
+namespace Webisell.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult FilterPage()
+        {
+            var vm = new FilterPageViewModel();
+            return View(vm);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Webisell.Web/Webisell.Web/ViewModels/FilterPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webisell.Web.ViewModels
+{
+    public class FilterPageViewModel
+    {
+        //Example
+        public List<FilterViewModel> Filters = new List<FilterViewModel>
+        {
+            new FilterViewModel
+            {
+                Name = "Brand",
+                AllowedValues = new List<string>{
+                    "Apple",
+                    "Nokia",
+                    "Samsung",
+                    "Sony"
+                }
+            },
+            new FilterViewModel
+            {
+                Name = "Camera",
+                AllowedValues = new List<string>{
+                    "2-8",
+                    "8-12",
+                    "12+"
+                }
+            }
+        };
+    }
+
+    public class FilterViewModel
+    {
+        public String Name { get; set; }
+        public List<String> AllowedValues;
+    }
+}
